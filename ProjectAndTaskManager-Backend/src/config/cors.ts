5b91f73a5c8594cd6c4d4f0bb07eba89f4a55165
Sebastian Galindo Zaragoza <133563476+Sebastian-Zaragoza@users.noreply.github.com>
2025-09-05 20:05:57 -0600

    feat: Fix small errors

--- ProjectAndTaskManager-Backend/src/config/cors.ts
+++ ProjectAndTaskManager-Backend/src/config/cors.ts
@@ -3,7 +3,7 @@ import { CorsOptions } from "cors";
 export const corsConfig: CorsOptions = {
     origin: (origin, callback) => {
         const whitelist = [process.env.FRONTEND_URL];
-        if (whitelist.includes(origin)) {
+        if (!whitelist.includes(origin)) {
             return callback(null, true);
         }
         return callback(new Error("Connection blocked by CORS"), false);
